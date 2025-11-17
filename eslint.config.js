7fa86ed47c21f09c5c2a7d23b4f559eaab1d0e07
Florian Stamer <github@stamer.one>
2025-11-17 20:44:14 +0100

    v1.0.4 release

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 
 // 1. Import the actual Prettier configuration objects
 import prettierConfig from 'eslint-config-prettier';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'; // Recommended way to include the plugin in flat config
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 const compat = new FlatCompat({
   baseDirectory: new URL('.', import.meta.url).pathname,
@@ -17,7 +17,7 @@ const compat = new FlatCompat({
 export default defineConfig([
   // 2. Start with the recommended base configs
   js.configs.recommended,
-  ...compat.extends('eslint:recommended'), // Still useful for compatibility if you have other legacy configs
+  ...compat.extends('eslint:recommended'),
 
   {
     languageOptions: {
@@ -31,18 +31,12 @@ export default defineConfig([
         HTMLElement: 'readonly',
       },
     },
-    // 3. Keep project-specific rules here
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
       'no-undef': 'off',
     },
   },
-
-  // 4. Add Prettier integration as the LAST item (it should override conflicting rules)
-  // This array combines the two core Prettier needs:
-  // a) Disabling conflicting rules (from eslint-config-prettier)
-  // b) Running Prettier as a rule (from eslint-plugin-prettier/recommended)
   prettierConfig,
   eslintPluginPrettier,
 ]);
